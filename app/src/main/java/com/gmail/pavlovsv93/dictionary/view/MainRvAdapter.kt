d9404c8bb6c05e28cf9c827e975908a74b47d7be
Pavlov Sergey <pavlov.androdev@gmail.com>
2022-06-04 20:25:11 +0300

    Добавлен Адаптер к RecyclerView

---
+++ app/src/main/java/com/gmail/pavlovsv93/dictionary/view/MainRvAdapter.kt
@@ -0,0 +1,47 @@
+package com.gmail.pavlovsv93.dictionary.view
+
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.gmail.pavlovsv93.dictionary.R
+import com.gmail.pavlovsv93.dictionary.databinding.ActivityMainRvItemBinding
+import com.gmail.pavlovsv93.dictionary.view.entityes.Word
+
+class MainRvAdapter() : RecyclerView.Adapter<MainRvAdapter.MainRvViewHolder>() {
+
+	inner class MainRvViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+		fun bind(word: Word) {
+			ActivityMainRvItemBinding.bind(itemView).apply {
+				tvHeaderItem.text = word.word
+				tvDescriptionItem.text = word.meanings.first().translation?.text
+
+			}
+		}
+
+	}
+
+	private val listData: MutableList<Word> = mutableListOf()
+
+	@SuppressLint("NotifyDataSetChanged")
+	fun setDataInRv(data: List<Word>) {
+		listData.clear()
+		listData.addAll(data)
+		notifyDataSetChanged()
+	}
+
+	override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainRvViewHolder {
+		return MainRvViewHolder(
+			LayoutInflater.from(parent.context)
+				.inflate(R.layout.activity_main_rv_item, parent, false)
+					as View
+		)
+	}
+
+	override fun onBindViewHolder(holder: MainRvViewHolder, position: Int) {
+		holder.bind(listData[position])
+	}
+
+	override fun getItemCount(): Int = listData.size
+}
