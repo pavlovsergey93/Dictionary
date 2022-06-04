9dd500eefd77a17bfede5b47b27dfce0cd842961
Pavlov Sergey <pavlov.androdev@gmail.com>
2022-06-04 19:36:14 +0300

    Добавлен репозиторий и Интераптор

---
+++ app/src/main/java/com/gmail/pavlovsv93/dictionary/data/repository/Repository.kt
@@ -0,0 +1,12 @@
+package com.gmail.pavlovsv93.dictionary.data.repository
+
+import com.gmail.pavlovsv93.dictionary.data.DataSourceInterface
+import com.gmail.pavlovsv93.dictionary.data.LocalDataSource
+import com.gmail.pavlovsv93.dictionary.view.entityes.Word
+import io.reactivex.rxjava3.core.Observable
+
+class Repository(private val dataSource: DataSourceInterface<List<Word>>) :
+	RepositoryInterface<List<Word>> {
+	override fun getDataRepository(word: String): Observable<List<Word>> =
+		dataSource.getData(word)
+}
