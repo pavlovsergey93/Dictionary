ffb7f9c63188e95b8d24b34c437b7bf933a0a3b4
Pavlov Sergey <pavlov.androdev@gmail.com>
2022-07-06 16:09:26 +0300

    Вынесены подключаемые библиотеки в отдельный фаил папки srcBuild

---
+++ buildSrc/src/main/kotlin/Deps.kt
@@ -0,0 +1,33 @@
+object Deps {
+	private const val ROOM_VERSION = "2.4.2"
+	const val ROOM_COMPILER = "androidx.room:room-compiler:$ROOM_VERSION"
+	const val ROOM_QUAVA = "androidx.room:room-guava:$ROOM_VERSION"
+	const val ROOM_KTX = "androidx.room:room-ktx:$ROOM_VERSION"
+
+	private const val RX_JAVA3_VERSION = "3.1.4"
+	const val RX_JAVA3 = "io.reactivex.rxjava3:rxjava:$RX_JAVA3_VERSION"
+	private const val RX_KOTLIN_VERSION = "3.0.1"
+	const val RX_KOTLIN = "io.reactivex.rxjava3:rxkotlin:$RX_KOTLIN_VERSION"
+	private const val RX_ANDROID_VERSION = "3.0.0"
+	const val RX_ANDROID = "io.reactivex.rxjava3:rxkotlin:$RX_ANDROID_VERSION"
+
+	private const val RETROFIT_VERSION = "2.9.0"
+	const val RETROFIT = "com.squareup.retrofit2:retrofit:$RETROFIT_VERSION"
+	const val CONVERTOR_GSON = "com.squareup.retrofit2:converter-gson:$RETROFIT_VERSION"
+	const val ADAPTER_RX_JAVA3 = "com.squareup.retrofit2:adapter-rxjava3:$RETROFIT_VERSION"
+
+	private const val OKHTTP_VERSION = "3.12.1"
+	const val OKHTTP = "com.squareup.okhttp3:logging-interceptor:$OKHTTP_VERSION"
+
+	private const val KOIN_VERSION = "3.1.6"
+	const val KOIN_ANDROID = "io.insert-koin:koin-android:$KOIN_VERSION"
+	const val KOIN_ANDROIDX_WORKMANAGER = "io.insert-koin:koin-androidx-workmanager:$KOIN_VERSION"
+	const val KOIN_ANDROIDX_NAVIGATION = "io.insert-koin:koin-androidx-navigation:$KOIN_VERSION"
+
+	private const val COROUTINES_VERSION = "1.5.2"
+	const val KOTLINX_COROUTINES_ANDROID = "org.jetbrains.kotlinx:kotlinx-coroutines-android:$COROUTINES_VERSION"
+
+	private const val GLIDE_VERSION = "4.13.0"
+	const val GLIDE = "com.github.bumptech.glide:glide:$GLIDE_VERSION"
+	const val GLIDE_COMPILER = "com.github.bumptech.glide:compiler:$GLIDE_VERSION"
+}
